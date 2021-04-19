535b3437d197a97036d9795a8da92339f574e83e
petrivanov <krest2005@yandex.ru>
2021-04-20 00:23:26 +0300

    fix tests

--- test/Funding.Test.ts
+++ test/Funding.Test.ts
@@ -18,8 +18,8 @@ describe("Funding", function() {
     })
 
     it("only owner can create campaign", async function () {
-        const startDate = (await latest()).add(duration.days(4))
-        const finishDate = (await latest()).add(duration.days(6))
+        const startDate = (await latest()).add(duration.days("4"))
+        const finishDate = (await latest()).add(duration.days("6"))
         const target = 30;
 
         await expect(this.funding.connect(this.bob).createCampaign(this.alice.address, target, startDate, finishDate))
@@ -29,8 +29,8 @@ describe("Funding", function() {
     })
 
     it("cannot create campaign with start date after end date", async function () {
-        const startDate = (await latest()).add(duration.days(4))
-        const finishDate = (await latest()).add(duration.days(3))
+        const startDate = (await latest()).add(duration.days("4"))
+        const finishDate = (await latest()).add(duration.days("3"))
         const target = 30;
 
         await expect(this.funding.createCampaign(this.alice.address, target, startDate, finishDate))
@@ -40,8 +40,8 @@ describe("Funding", function() {
     })
 
     it("cannot create campaign with zero address owner", async function () {
-        const startDate = (await latest()).add(duration.days(4))
-        const finishDate = (await latest()).add(duration.days(5))
+        const startDate = (await latest()).add(duration.days("4"))
+        const finishDate = (await latest()).add(duration.days("5"))
         const target = 30;
 
         await expect(this.funding.createCampaign(ADDRESS_ZERO, target, startDate, finishDate))
@@ -51,8 +51,8 @@ describe("Funding", function() {
     })
 
     it("cannot create campaign with zero target", async function () {
-        const startDate = (await latest()).add(duration.days(4))
-        const finishDate = (await latest()).add(duration.days(5))
+        const startDate = (await latest()).add(duration.days("4"))
+        const finishDate = (await latest()).add(duration.days("5"))
         const target = 0;
 
         await expect(this.funding.createCampaign(this.alice.address, target, startDate, finishDate))
@@ -62,8 +62,8 @@ describe("Funding", function() {
     })
 
     it("cannot create campaign with start date in the past", async function () {
-        const startDate = (await latest()).sub(duration.days(4))
-        const finishDate = (await latest()).add(duration.days(3))
+        const startDate = (await latest()).sub(duration.days("4"))
+        const finishDate = (await latest()).add(duration.days("3"))
         const target = 30;
 
         await expect(this.funding.createCampaign(this.alice.address, target, startDate, finishDate))
@@ -74,8 +74,8 @@ describe("Funding", function() {
 
     it("createdCampaign appears in campaign list", async function() {
         //create campaign
-        const startDate = (await latest()).add(duration.days(4))
-        const finishDate = (await latest()).add(duration.days(6))
+        const startDate = (await latest()).add(duration.days("4"))
+        const finishDate = (await latest()).add(duration.days("6"))
         const target = 30;
         await this.funding.createCampaign(this.bob.address, target, startDate, finishDate)
         //retrieve created campaign
@@ -91,8 +91,8 @@ describe("Funding", function() {
 
     it("cannot fund not started campaign", async function (){
         //create campaign
-        const startDate = (await latest()).add(duration.days(4))
-        const finishDate = (await latest()).add(duration.days(6))
+        const startDate = (await latest()).add(duration.days("4"))
+        const finishDate = (await latest()).add(duration.days("6"))
         const target = 30;
         await this.funding.createCampaign(this.bob.address, target, startDate, finishDate)
         //expect fail funding not started campaign
@@ -101,24 +101,24 @@ describe("Funding", function() {
 
     it("owner cannot fund its own campaign", async function () {
         //create campaign
-        const startDate = (await latest()).add(duration.days(2))
-        const finishDate = (await latest()).add(duration.days(6))
+        const startDate = (await latest()).add(duration.days("2"))
+        const finishDate = (await latest()).add(duration.days("6"))
         const target = 30;
         await this.funding.createCampaign(this.bob.address, target, startDate, finishDate)
         //advance time to after finish date
-        await increase(duration.days(5))
+        await increase(duration.days("5"))
         //expect fail funding finished campaign
         await expect(this.funding.connect(this.bob).fund(0, {value: 15})).to.be.revertedWith("Owner cannot fund its own campaign")
     })
 
     it("cannot fund finished campaign", async function (){
         //create campaign
-        const startDate = (await latest()).add(duration.days(4))
-        const finishDate = (await latest()).add(duration.days(6))
+        const startDate = (await latest()).add(duration.days("4"))
+        const finishDate = (await latest()).add(duration.days("6"))
         const target = 30;
         await this.funding.createCampaign(this.bob.address, target, startDate, finishDate)
         //advance time to after finish date
-        await increase(duration.days(7))
+        await increase(duration.days("7"))
         //expect fail funding finished campaign
         await expect(this.funding.fund(0, {value: 15})).to.be.revertedWith("Campaign is already finished")
     })
@@ -128,12 +128,12 @@ describe("Funding", function() {
     })
 
     it("fund campaign correctly", async function () {
-        const startDate = (await latest()).add(duration.days(4))
-        const finishDate = (await latest()).add(duration.days(6))
+        const startDate = (await latest()).add(duration.days("4"))
+        const finishDate = (await latest()).add(duration.days("6"))
         const target = 30;
         await this.funding.createCampaign(this.bob.address, target, startDate, finishDate)
         //advance time after start date
-        await increase(duration.days(5))
+        await increase(duration.days("5"))
         //fund campaign
         const fundSum = 100
         const beforeBalance = await ethers.provider.getBalance(this.funding.address)
@@ -154,34 +154,34 @@ describe("Funding", function() {
 
     it("funder cannot withdraw from successful campaign", async function() {
         //create campaign
-        const startDate = (await latest()).add(duration.days(3))
-        const finishDate = (await latest()).add(duration.days(6))
+        const startDate = (await latest()).add(duration.days("3"))
+        const finishDate = (await latest()).add(duration.days("6"))
         const target = 30;
         await this.funding.createCampaign(this.bob.address, target, startDate, finishDate)
-        await increase(duration.days(5))
+        await increase(duration.days("5"))
         //fulfill campaign
         const fundSum = 100
         await this.funding.connect(this.alice).fund(0, {value: fundSum})
         //advance campaign to finish
-        await increase(duration.days(2))
+        await increase(duration.days("2"))
         //expect withdraw by funder failed
         await expect(this.funding.withdraw(0)).to.be.revertedWith("Campaign is succeeded, cannot collect funds by funders")
     })
 
     it("funder can withdraw from failed campaign", async function() {
         //create campaign
-        const startDate = (await latest()).add(duration.days(1))
-        const finishDate = (await latest()).add(duration.days(6))
+        const startDate = (await latest()).add(duration.days("1"))
+        const finishDate = (await latest()).add(duration.days("6"))
         const target = 30;
         await this.funding.createCampaign(this.bob.address, target, startDate, finishDate)
         //advance campaign active state
-        await increase(duration.days(5))
+        await increase(duration.days("5"))
         //fulfill campaign partially
         const fundSum = 10
         await this.funding.connect(this.alice).fund(0, {value: fundSum})
         await this.funding.connect(this.alice).fund(0, {value: 5})
         //advance campaign to finish
-        await increase(duration.days(5))
+        await increase(duration.days("5"))
         //expect withdraw by funder
         const beforeBalance = await ethers.provider.getBalance(this.funding.address)
         await this.funding.connect(this.alice).withdraw(0)
@@ -193,34 +193,34 @@ describe("Funding", function() {
 
     it("owner cannot withdraw from failed campaign", async function() {
         //create campaign
-        const startDate = (await latest()).add(duration.days(1))
-        const finishDate = (await latest()).add(duration.days(6))
+        const startDate = (await latest()).add(duration.days("1"))
+        const finishDate = (await latest()).add(duration.days("6"))
         const target = 30;
         await this.funding.createCampaign(this.bob.address, target, startDate, finishDate)
         //advance campaign active state
-        await increase(duration.days(5))
+        await increase(duration.days("5"))
         //fulfill campaign partialy
         const fundSum = 10
         await this.funding.connect(this.alice).fund(0, {value: fundSum})
         //advance campaign to finish
-        await increase(duration.days(2))
+        await increase(duration.days("2"))
         //expect withdraw by owner failed
         await expect(this.funding.connect(this.bob).withdraw(0)).to.be.revertedWith("Campaign is failed, cannot collect funds by owner")
     })
 
     it("owner can withdraw from successful campaign", async function() {
         //create campaign
-        const startDate = (await latest()).add(duration.days(1))
-        const finishDate = (await latest()).add(duration.days(6))
+        const startDate = (await latest()).add(duration.days("1"))
+        const finishDate = (await latest()).add(duration.days("6"))
         const target = 30;
         await this.funding.createCampaign(this.bob.address, target, startDate, finishDate)
         //advance campaign active state
-        await increase(duration.days(5))
+        await increase(duration.days("5"))
         //fulfill campaign
         const fundSum = 100
         await this.funding.connect(this.alice).fund(0, {value: fundSum})
         //advance campaign to finish
-        await increase(duration.days(2))
+        await increase(duration.days("2"))
         //expect withdraw by owner
         const beforeBalance = await ethers.provider.getBalance(this.funding.address)
         await this.funding.connect(this.bob).withdraw(0)
@@ -232,32 +232,32 @@ describe("Funding", function() {
 
     it("cannot withdraw from in progress or not started campaign", async function(){
         //create campaign
-        const startDate = (await latest()).add(duration.days(6))
-        const finishDate = (await latest()).add(duration.days(8))
+        const startDate = (await latest()).add(duration.days("6"))
+        const finishDate = (await latest()).add(duration.days("8"))
         const target = 30;
         await this.funding.createCampaign(this.bob.address, target, startDate, finishDate)
         //expect withdraw failed from not started campaign
         await expect(this.funding.connect(this.bob).withdraw(0)).to.be.revertedWith("Campaign is not finished yet")
         //advance campaign active state
-        await increase(duration.days(7))
+        await increase(duration.days("7"))
         //expect withdraw failed from in progress campaign
         await expect(this.funding.connect(this.bob).withdraw(0)).to.be.revertedWith("Campaign is not finished yet")
     })
 
     it("funders cannot withdraw from campaign where they have no funding", async function(){
         //create campaign
-        const startDate = (await latest()).add(duration.days(1))
-        const finishDate = (await latest()).add(duration.days(6))
+        const startDate = (await latest()).add(duration.days("1"))
+        const finishDate = (await latest()).add(duration.days("6"))
         const target = 30;
         await this.funding.createCampaign(this.bob.address, target, startDate, finishDate)
         //advance campaign active state
-        await increase(duration.days(5))
+        await increase(duration.days("5"))
         //fulfill campaign partially
         const fundSum = 10
         await this.funding.connect(this.alice).fund(0, {value: fundSum})
         await this.funding.connect(this.alice).fund(0, {value: 5})
         //advance campaign to finish
-        await increase(duration.days(5))
+        await increase(duration.days("5"))
         //cannot withdarw by third person
         await expect(this.funding.connect(this.carol).withdraw(0)).to.be.revertedWith("Nothing to withdraw")
     })
--- test/utils/time.ts
+++ test/utils/time.ts
@@ -1,6 +1,8 @@
-const { ethers } = require("hardhat")
+import {BigNumber} from "ethers";
+import {ethers} from "hardhat";
 
-const { BigNumber } = ethers
+//const { ethers } = require("hardhat")
+
 
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
 
@@ -8,48 +10,48 @@ export async function advanceBlock() {
   return ethers.provider.send("evm_mine", [])
 }
 
-export async function advanceBlockTo(blockNumber) {
+export async function advanceBlockTo( blockNumber: number ): Promise<void> {
   for (let i = await ethers.provider.getBlockNumber(); i < blockNumber; i++) {
     await advanceBlock()
   }
 }
 
-export async function increase(value) {
+export async function increase(value: BigNumber): Promise<void> {
   await ethers.provider.send("evm_increaseTime", [value.toNumber()])
   await advanceBlock()
 }
 
-export async function latest() {
+export async function latest(): Promise<BigNumber> {
   const block = await ethers.provider.getBlock("latest")
   return BigNumber.from(block.timestamp)
 }
 
-export async function advanceTimeAndBlock(time) {
+export async function advanceTimeAndBlock(time: BigNumber): Promise<void> {
   await advanceTime(time)
   await advanceBlock()
 }
 
-export async function advanceTime(time) {
+export async function advanceTime(time: BigNumber): Promise<void> {
   await ethers.provider.send("evm_increaseTime", [time])
 }
 
 export const duration = {
-  seconds: function (val) {
+  seconds: function (val: string): BigNumber {
     return BigNumber.from(val)
   },
-  minutes: function (val) {
+  minutes: function (val: string): BigNumber {
     return BigNumber.from(val).mul(this.seconds("60"))
   },
-  hours: function (val) {
+  hours: function (val: string): BigNumber {
     return BigNumber.from(val).mul(this.minutes("60"))
   },
-  days: function (val) {
+  days: function (val: string): BigNumber {
     return BigNumber.from(val).mul(this.hours("24"))
   },
-  weeks: function (val) {
+  weeks: function (val: string): BigNumber {
     return BigNumber.from(val).mul(this.days("7"))
   },
-  years: function (val) {
+  years: function (val: string): BigNumber {
     return BigNumber.from(val).mul(this.days("365"))
   },
 }
